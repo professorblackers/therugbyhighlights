b86066363700dee82f8bdfc85c577d63ae6b5635
Robert Blackburn <professorblackers@gmail.com>
2024-03-28 14:04:20 +0100

    Install encore, twig and set up basic page

---
+++ assets/pages/rugby.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<script>
+export default {
+  name: 'Rugby',
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  created() {
+
+  }
+}
+</script>
